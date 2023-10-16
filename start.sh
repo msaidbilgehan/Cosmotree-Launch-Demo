61307501eaadbea19424d8f970187f7fdc0beffa
Muhammed Said BİLGEHAN <msaidbilgehan@gmail.com>
2023-10-16 16:44:36 +0300

    start script added

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Dizine git
+cd ~/Cosmotree_Experiment/Docker
+
+# Python Flask uygulamasını başlat
+python flask_app.py &
+
+# Chromium'u belirtilen adreste aç
+# Birkaç saniye bekleme süresi ekledik, Flask uygulamasının başlaması için
+sleep 2
+chromium-browser 127.0.0.1:5005 &
+
+exit 0
